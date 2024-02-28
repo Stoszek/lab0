d19c56dfef2667bc087e9f55e83081050c6667d2
micwyr0895 <micwyr0895@W04N-P317D14>
2024-02-28 17:48:56 +0100

    Add project files.

---
+++ lab0/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab0
+{
+    
+    internal class FizzBuzz
+    {
+        public int upperLimit;
+
+        internal FizzBuzz(int upperLimit)
+        {
+            this.upperLimit = upperLimit;
+        }
+        public void Display()
+        {
+            for (int i = 0; i < upperLimit; i++)
+            {
+
+            }
+        }
+    }
+
+}
+
